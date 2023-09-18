6ea7d4c209864f6380ee3965af28e81ccf7e1f43
Daniel van de Velden <d-van-de-velden@users.noreply.github.com>
2023-09-18 10:00:17 +0200

    Update __init__.py

--- scripts/viz/__init__.py
+++ scripts/viz/__init__.py
@@ -1,7 +1,12 @@
-# Authors (alphabetical order):
-#          Daniel van de Velden <daniel.vandevelden@yahoo.de>
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Authors: Daniel van de Velden (d.vandevelden@yahoo.de)
 #
-# License: Simplified BSD
+# License: BSD (3-clause)
+# 
+# 
+#
+###############################################################
 
 
 from . import combine_plots
