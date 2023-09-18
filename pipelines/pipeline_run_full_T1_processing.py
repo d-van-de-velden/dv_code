d4ec5c480a59d2fff3c3954e14cf3c13a504906d
Daniel van de Velden <d-van-de-velden@users.noreply.github.com>
2023-09-18 09:59:43 +0200

    Update pipeline_run_full_T1_processing.py

--- pipelines/pipeline_run_full_T1_processing.py
+++ pipelines/pipeline_run_full_T1_processing.py
@@ -1,1 +1,9 @@
-print('Dummy')
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Authors: Daniel van de Velden (d.vandevelden@yahoo.de)
+#
+# License: BSD (3-clause)
+# 
+# 
+#
+###############################################################
