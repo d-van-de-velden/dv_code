9424dc2cc0d14d82b59ae52d44ada13774a30424
Daniel van de Velden <d-van-de-velden@users.noreply.github.com>
2023-09-18 10:01:03 +0200

    Update __init__.py

--- scripts/misc/__init__.py
+++ scripts/misc/__init__.py
@@ -1,22 +1,21 @@
 """Miscellaneous for any analysis."""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Authors: Daniel van de Velden (d.vandevelden@yahoo.de)
 #
+# License: BSD (3-clause)
+# 
+# 
 #
+###############################################################
 
 from .analysis_feedback import loadingBar
-
 from .check_make_dir    import check_make_dir
-
 from .feedback_messages import error_message, warn_message
-
 from .get_containerIMG import get_container_fMRIprep, get_container_Freesurfer
-
 from .read_analysis_params import read_analysis_params
-
 from .select_filetype import get_only_json, get_only_nifti, get_only_zip
-
 from .setup_BIDS_dir import setup_BIDS_dir
-
 from .work_matrix import rotate_matrix
-
 from .use_utils import unpack_compressed
 
