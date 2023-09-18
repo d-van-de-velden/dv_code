f7b11223d475ea1f9ec503b4965939c7f53f58d3
Daniel van de Velden <d-van-de-velden@users.noreply.github.com>
2023-09-18 10:01:36 +0200

    Update __init__.py

--- scripts/hpc/__init__.py
+++ scripts/hpc/__init__.py
@@ -1,5 +1,12 @@
-"""Miscellaneous for any analysis."""
+"""Functionalities to submit any python function to a cluster node."""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Authors: Daniel van de Velden (d.vandevelden@yahoo.de)
 #
+# License: BSD (3-clause)
+# 
+# 
 #
+###############################################################
 
 from .submit_job import submit_job
