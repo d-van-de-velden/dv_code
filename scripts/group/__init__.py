a4854dd1d3bb405397c6ca39252bd86d92b88bdd
Daniel van de Velden <d-van-de-velden@users.noreply.github.com>
2023-09-18 10:01:53 +0200

    Update __init__.py

--- scripts/group/__init__.py
+++ scripts/group/__init__.py
@@ -1,6 +1,13 @@
-"""Miscellaneous for any analysis."""
+"""Analysis on group level."""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Authors: Daniel van de Velden (d.vandevelden@yahoo.de)
 #
+# License: BSD (3-clause)
+# 
+# 
 #
+###############################################################
 
 from .analyze_group import overview_tSNR
 
